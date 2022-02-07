3fb31e9c7c61de72ff01d315ceeec8649fe47a25
geekfabio <geekfabio@gmail.com>
2022-02-07 22:26:42 +0100

    add desktop view

--- lib/todo_bloc/presentation/screens/home/home_page.dart
+++ lib/todo_bloc/presentation/screens/home/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:todo_bloc/todo_bloc/presentation/components/menu/menu.dart';
 import 'package:todo_bloc/todo_bloc/presentation/screens/home/components/list_of_todos.dart';
+import 'package:todo_bloc/todo_bloc/shared/themes/theme.dart';
 import 'package:todo_bloc/todo_bloc/shared/utils/responsive.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,11 +14,20 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Responsive(
-        desktop: ListOfTodos(),
-        tablet: ListOfTodos(),
-        mobile: ListOfTodos(),
+        desktop: Row(
+          children: [
+            Expanded(
+                flex: AppTheme.screenWidth(context) > 1345 ? 2 : 1,
+                child: const Menu()),
+            Expanded(
+                flex: AppTheme.screenWidth(context) > 1345 ? 7 : 4,
+                child: const ListOfTodos()),
+          ],
+        ),
+        tablet: const ListOfTodos(),
+        mobile: const ListOfTodos(),
       ),
     );
   }
