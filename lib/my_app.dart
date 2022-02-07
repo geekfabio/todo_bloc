b618ebcfc90585ffb9f49e7de5a886af2ab62c0d
geekfabio <geekfabio@gmail.com>
2022-02-07 20:43:56 +0100

    ♻

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo_bloc/todo_bloc/cubit/theme_cubit.dart';
+import 'package:todo_bloc/todo_bloc/presentation/screens/home/home_page.dart';
 import 'package:todo_bloc/todo_bloc/presentation/screens/signup/signup_page.dart';
 import 'package:todo_bloc/todo_bloc/shared/themes/theme.dart';
 
@@ -14,10 +15,11 @@ class MyApp extends StatelessWidget {
         builder: (context, state) {
           return MaterialApp(
             theme: AppTheme.lightTheme,
-            themeMode: context.select((ThemeCubit cubit) => cubit.state.mode),
+            //    themeMode: context.select((ThemeCubit cubit) => cubit.state.mode),
+            themeMode: ThemeMode.light,
             darkTheme: AppTheme.darkTheme,
             title: 'Autocompletar form com NIF/ID',
-            home: const SignupPage(),
+            home: const HomePage(),
           );
         },
       ),
