0d07b5a5fd86d552c6d1ed57bfe10dce09045395
geekfabio <geekfabio@gmail.com>
2022-02-07 20:42:21 +0100

    ♻

--- lib/todo_bloc/shared/themes/light_color.dart
+++ lib/todo_bloc/shared/themes/light_color.dart
@@ -4,18 +4,20 @@ class LightColor {
   ///Theme light
   static const Color primaryColor = Color(0xFF1758C1);
   static const Color splashBackground = Color(0XFFFFFFFF);
-  static const Color background = Color(0XFFFFFFFF);
+  static const Color background = Color(0xFFF7F7F9);
 
   //text colors
   static const Color titleTextColor = Color(0xFF2D333A);
   static const Color subTitleTextColor = Color(0xFF505255);
   static const Color bodyTextCaption = Color(0xff20262C);
 
+  //card Colors
+
   //Input Theme
   static const Color borderColor = Color(0xFFF9F9F9);
   static const Color focusedBorderColor = lightBlue;
-  static const Color focusColor = primaryColor;
-  static const Color inputFillColor = Color(0xFFF2F6FC);
+  static const Color focusColor = splashBackground;
+  static const Color inputFillColor = splashBackground;
   static const Color inputHoverColor = Color(0xFFF2F6FC);
   static const LinearGradient colorGradientGrey = LinearGradient(
     begin: Alignment.bottomLeft,
