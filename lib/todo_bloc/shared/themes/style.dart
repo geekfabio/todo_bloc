240fca180eab29a622b994360925ddff3b68adfc
geekfabio <geekfabio@gmail.com>
2022-02-07 20:43:02 +0100

    remove verySlow Transition

--- lib/todo_bloc/shared/themes/style.dart
+++ lib/todo_bloc/shared/themes/style.dart
@@ -9,7 +9,7 @@ class AppTransitionTimes {
   static const fast = Duration(milliseconds: 150);
   static const medium = Duration(milliseconds: 300);
   static const slow = Duration(milliseconds: 600);
-  static const verySlow = Duration(seconds: 10);
+  // static const verySlow = Duration(seconds: 10);
 }
 
 class AppIconSizes {
@@ -24,7 +24,7 @@ class Insets {
   static const double xs = 4;
   static const double sm = 8;
   static const double md = 12;
-  static const double lg = 16;
+  static const double lg = 20;
   static const double xl = 32;
 
   static const double offset = 40;
@@ -59,9 +59,9 @@ class Shadows {
       ];
   static List<BoxShadow> get small => [
         BoxShadow(
-            color: const Color(0xff333333).withOpacity(.15),
-            spreadRadius: 0,
-            blurRadius: 3,
+            color: const Color(0xff333333).withOpacity(.05),
+            spreadRadius: 0.1,
+            blurRadius: 0.2,
             offset: const Offset(0, 1)),
       ];
 }
