252a7835e16f3fb92f747d338052b0772246a516
geekfabio <geekfabio@gmail.com>
2022-02-08 23:00:21 +0100

    unecessary imports

--- lib/todo_bloc/presentation/screens/home/components/todo_navbar/todo_navbar.dart
+++ lib/todo_bloc/presentation/screens/home/components/todo_navbar/todo_navbar.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_bloc/todo_bloc/shared/themes/style.dart';
 import 'package:todo_bloc/todo_bloc/shared/widgets/ripple_extension.dart';
@@ -18,7 +19,24 @@ class TodoNavBar extends StatelessWidget {
           child: Container(
             color: Theme.of(context).scaffoldBackgroundColor,
             child: Column(
-              children: [TodoContent()],
+              children: [
+                TodoContent(),
+                Container(
+                  padding: const EdgeInsets.only(bottom: 15),
+                  decoration: BoxDecoration(
+                    border: Border(
+                      bottom:
+                          BorderSide(color: Theme.of(context).backgroundColor),
+                    ),
+                  ),
+                  child: Row(
+                    children: [
+                      const SizedBox(width: Insets.lg * 0.75),
+                      const Spacer(),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
         ),
@@ -40,58 +58,59 @@ class TodoContent extends StatelessWidget {
           Row(
             children: <Widget>[
               // check box To change when complete task
-              AnimatedContainer(
-                  duration: AppTransitionTimes.medium,
-                  curve: Curves.bounceInOut,
-                  margin: const EdgeInsets.only(left: 0),
-                  height: 20.0,
-                  width: 20.0,
-                  decoration: BoxDecoration(
-                    borderRadius: const BorderRadius.all(Radius.circular(10)),
-                    color: Colors.white,
-                    border: Border.all(
-                      color: Colors.grey,
-                      width: 0.51,
+              Align(
+                alignment: AlignmentDirectional.center,
+                heightFactor: 0,
+                child: AnimatedContainer(
+                    duration: AppTransitionTimes.medium,
+                    curve: Curves.bounceInOut,
+                    margin: const EdgeInsets.only(left: 0),
+                    height: 20.0,
+                    width: 20.0,
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.all(Radius.circular(10)),
+                      color: Colors.white,
+                      border: Border.all(
+                        color: Colors.grey,
+                        width: 0.51,
+                      ),
                     ),
-                  ),
-                  child: //widget.model.isChecked ?
-                      Center(
-                    child: Icon(Icons.check,
-                        size: 14.0, color: Theme.of(context).primaryColor),
-                  )
-                  // : Container(),
-                  ).ripple(() {}),
+                    child: //widget.model.isChecked ?
+                        Center(
+                      child: Icon(Icons.check,
+                          size: 14.0, color: Theme.of(context).primaryColor),
+                    )
+                    // : Container(),
+                    ).ripple(() {}),
+              ),
 
               // text
               Flexible(
-                fit: FlexFit.tight,
+                fit: FlexFit.loose,
                 child: Row(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   mainAxisAlignment: MainAxisAlignment.start,
-                  children: const <Widget>[
-                    SizedBox(
-                      width: 10,
-                    ),
+                  children: <Widget>[
                     Expanded(
-                      child: Text(
-                        "widget.model.title",
-                        overflow: TextOverflow.ellipsis,
-                        softWrap: true,
+                      flex: 6,
+                      child: TextField(
+                        minLines: 1,
+                        maxLines: 3,
+                        onChanged: (value) {},
+                        decoration: InputDecoration(
+                          fillColor: Theme.of(context).scaffoldBackgroundColor,
+                          hintText: "Edit a task",
+                          filled: true,
+                          border: InputBorder.none,
+                          focusedBorder: InputBorder.none,
+                        ),
                       ),
                     ),
-                    SizedBox(
+                    const SizedBox(
                       width: 6,
                     ),
+
                     //  if (widget.model.tag != null)
-                    Expanded(
-                      child: Text(
-                        "",
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(color: Colors.red),
-                        softWrap: true,
-                        textAlign: TextAlign.right,
-                      ),
-                    ),
                   ],
                 ),
               ),
