30b5f03125d4c2b622b987d0dfd309c0675797d0
geekfabio <geekfabio@gmail.com>
2022-02-10 21:01:22 +0100

    add enum TodoPriority

--- lib/todo_bloc/presentation/screens/models/todo_model.dart
+++ lib/todo_bloc/presentation/screens/models/todo_model.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
+enum TodoPriority {
+  high,
+  medium,
+  low,
+}
+
 class TodoModel {
   String title;
   String dateCreated;
@@ -10,6 +16,7 @@ class TodoModel {
   Color? color;
   bool isChecked;
   List<TaskModel>? subtasks;
+  TodoPriority priority;
   TodoModel({
     required this.title,
     required this.dateCreated,
@@ -20,6 +27,7 @@ class TodoModel {
     this.path = "",
     this.color = Colors.blue,
     this.subtasks,
+    this.priority = TodoPriority.low,
   });
 }
 
@@ -55,276 +63,6 @@ List<TodoModel> todos = List.generate(
 
 //add more itens to test responsivity
 List demo_data = [
-  {
-    "name": "Apple",
-    "image": TagModel(title: "Software", color: Colors.purple),
-    "subject": "iPhone 12 is here",
-    "isAttachmentAvailable": false,
-    "isChecked": true,
-    "tagColor": Color(0xFF23CF91),
-    "time": "Now"
-  },
-  {
-    "name": "Elvia Atkins",
-    "image": TagModel(title: "Project", color: Colors.red),
-    "subject": "Inspiration for our new home",
-    "isAttachmentAvailable": true,
-    "isChecked": false,
-    "tagColor": null,
-    "time": "15:32"
-  },
-  {
-    "name": "Marvin Kiehn",
-    "image": TagModel(title: "Project", color: Colors.red),
-    "subject": "Business-focused empowering the world",
-    "isAttachmentAvailable": true,
-    "isChecked": false,
-    "tagColor": null,
-    "time": "14:27",
-  },
-  {
-    "name": "Domenic Bosco",
-    "image": TagModel(title: "Green", color: Colors.green),
-    "subject": "The fastest way to Design",
-    "isAttachmentAvailable": false,
-    "isChecked": true,
-    "tagColor": Color(0xFF23CF91),
-    "time": "10:43"
-  },
-  {
-    "name": "Elenor Bauch",
-    "image": null,
-    "subject": "New job opportunities",
-    "isAttachmentAvailable": false,
-    "isChecked": false,
-    "tagColor": Color(0xFF3A6FF7),
-    "time": "9:58"
-  },
-  {
-    "name": "Apple",
-    "image": TagModel(title: "Software", color: Colors.purple),
-    "subject": "iPhone 12 is here",
-    "isAttachmentAvailable": false,
-    "isChecked": true,
-    "tagColor": Color(0xFF23CF91),
-    "time": "Now"
-  },
-  {
-    "name": "Elvia Atkins",
-    "image": TagModel(title: "Project", color: Colors.red),
-    "subject": "Inspiration for our new home",
-    "isAttachmentAvailable": true,
-    "isChecked": false,
-    "tagColor": null,
-    "time": "15:32"
-  },
-  {
-    "name": "Marvin Kiehn",
-    "image": TagModel(title: "Project", color: Colors.red),
-    "subject": "Business-focused empowering the world",
-    "isAttachmentAvailable": true,
-    "isChecked": false,
-    "tagColor": null,
-    "time": "14:27",
-  },
-  {
-    "name": "Domenic Bosco",
-    "image": TagModel(title: "Green", color: Colors.green),
-    "subject": "The fastest way to Design",
-    "isAttachmentAvailable": false,
-    "isChecked": true,
-    "tagColor": Color(0xFF23CF91),
-    "time": "10:43"
-  },
-  {
-    "name": "Elenor Bauch",
-    "image": null,
-    "subject": "New job opportunities",
-    "isAttachmentAvailable": false,
-    "isChecked": false,
-    "tagColor": Color(0xFF3A6FF7),
-    "time": "9:58"
-  },
-  {
-    "name": "Apple",
-    "image": TagModel(title: "Software", color: Colors.purple),
-    "subject": "iPhone 12 is here",
-    "isAttachmentAvailable": false,
-    "isChecked": true,
-    "tagColor": Color(0xFF23CF91),
-    "time": "Now"
-  },
-  {
-    "name": "Elvia Atkins",
-    "image": TagModel(title: "Project", color: Colors.red),
-    "subject": "Inspiration for our new home",
-    "isAttachmentAvailable": true,
-    "isChecked": false,
-    "tagColor": null,
-    "time": "15:32"
-  },
-  {
-    "name": "Marvin Kiehn",
-    "image": TagModel(title: "Project", color: Colors.red),
-    "subject": "Business-focused empowering the world",
-    "isAttachmentAvailable": true,
-    "isChecked": false,
-    "tagColor": null,
-    "time": "14:27",
-  },
-  {
-    "name": "Domenic Bosco",
-    "image": TagModel(title: "Green", color: Colors.green),
-    "subject": "The fastest way to Design",
-    "isAttachmentAvailable": false,
-    "isChecked": true,
-    "tagColor": Color(0xFF23CF91),
-    "time": "10:43"
-  },
-  {
-    "name": "Elenor Bauch",
-    "image": null,
-    "subject": "New job opportunities",
-    "isAttachmentAvailable": false,
-    "isChecked": false,
-    "tagColor": Color(0xFF3A6FF7),
-    "time": "9:58"
-  },
-  {
-    "name": "Apple",
-    "image": TagModel(title: "Software", color: Colors.purple),
-    "subject": "iPhone 12 is here",
-    "isAttachmentAvailable": false,
-    "isChecked": true,
-    "tagColor": Color(0xFF23CF91),
-    "time": "Now"
-  },
-  {
-    "name": "Elvia Atkins",
-    "image": TagModel(title: "Project", color: Colors.red),
-    "subject": "Inspiration for our new home",
-    "isAttachmentAvailable": true,
-    "isChecked": false,
-    "tagColor": null,
-    "time": "15:32"
-  },
-  {
-    "name": "Marvin Kiehn",
-    "image": TagModel(title: "Project", color: Colors.red),
-    "subject": "Business-focused empowering the world",
-    "isAttachmentAvailable": true,
-    "isChecked": false,
-    "tagColor": null,
-    "time": "14:27",
-  },
-  {
-    "name": "Domenic Bosco",
-    "image": TagModel(title: "Green", color: Colors.green),
-    "subject": "The fastest way to Design",
-    "isAttachmentAvailable": false,
-    "isChecked": true,
-    "tagColor": Color(0xFF23CF91),
-    "time": "10:43"
-  },
-  {
-    "name": "Elenor Bauch",
-    "image": null,
-    "subject": "New job opportunities",
-    "isAttachmentAvailable": false,
-    "isChecked": false,
-    "tagColor": Color(0xFF3A6FF7),
-    "time": "9:58"
-  },
-  {
-    "name": "Apple",
-    "image": TagModel(title: "Software", color: Colors.purple),
-    "subject": "iPhone 12 is here",
-    "isAttachmentAvailable": false,
-    "isChecked": true,
-    "tagColor": Color(0xFF23CF91),
-    "time": "Now"
-  },
-  {
-    "name": "Elvia Atkins",
-    "image": TagModel(title: "Project", color: Colors.red),
-    "subject": "Inspiration for our new home",
-    "isAttachmentAvailable": true,
-    "isChecked": false,
-    "tagColor": null,
-    "time": "15:32"
-  },
-  {
-    "name": "Marvin Kiehn",
-    "image": TagModel(title: "Project", color: Colors.red),
-    "subject": "Business-focused empowering the world",
-    "isAttachmentAvailable": true,
-    "isChecked": false,
-    "tagColor": null,
-    "time": "14:27",
-  },
-  {
-    "name": "Domenic Bosco",
-    "image": TagModel(title: "Green", color: Colors.green),
-    "subject": "The fastest way to Design",
-    "isAttachmentAvailable": false,
-    "isChecked": true,
-    "tagColor": Color(0xFF23CF91),
-    "time": "10:43"
-  },
-  {
-    "name": "Elenor Bauch",
-    "image": null,
-    "subject": "New job opportunities",
-    "isAttachmentAvailable": false,
-    "isChecked": false,
-    "tagColor": Color(0xFF3A6FF7),
-    "time": "9:58"
-  },
-  {
-    "name": "Apple",
-    "image": TagModel(title: "Software", color: Colors.purple),
-    "subject": "iPhone 12 is here",
-    "isAttachmentAvailable": false,
-    "isChecked": true,
-    "tagColor": Color(0xFF23CF91),
-    "time": "Now"
-  },
-  {
-    "name": "Elvia Atkins",
-    "image": TagModel(title: "Project", color: Colors.red),
-    "subject": "Inspiration for our new home",
-    "isAttachmentAvailable": true,
-    "isChecked": false,
-    "tagColor": null,
-    "time": "15:32"
-  },
-  {
-    "name": "Marvin Kiehn",
-    "image": TagModel(title: "Project", color: Colors.red),
-    "subject": "Business-focused empowering the world",
-    "isAttachmentAvailable": true,
-    "isChecked": false,
-    "tagColor": null,
-    "time": "14:27",
-  },
-  {
-    "name": "Domenic Bosco",
-    "image": TagModel(title: "Green", color: Colors.green),
-    "subject": "The fastest way to Design",
-    "isAttachmentAvailable": false,
-    "isChecked": true,
-    "tagColor": Color(0xFF23CF91),
-    "time": "10:43"
-  },
-  {
-    "name": "Elenor Bauch",
-    "image": null,
-    "subject": "New job opportunities",
-    "isAttachmentAvailable": false,
-    "isChecked": false,
-    "tagColor": Color(0xFF3A6FF7),
-    "time": "9:58"
-  },
   {
     "name": "Apple",
     "image": TagModel(title: "Software", color: Colors.purple),
