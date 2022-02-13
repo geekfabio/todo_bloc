761476c57db2684ee3e86ca2f29a0a4ecd70e8e2
geekfabio <geekfabio@gmail.com>
2022-02-13 12:31:47 +0100

    change folder name

--- lib/todo_bloc/cubit/theme_state.dart
+++
@@ -1,9 +0,0 @@
-part of 'theme_cubit.dart';
-
-class ThemeState extends Equatable {
-  const ThemeState({required this.mode});
-  final ThemeMode mode;
-
-  @override
-  List<Object> get props => [mode];
-}
