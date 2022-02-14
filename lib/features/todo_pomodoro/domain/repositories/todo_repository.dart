862099d83db993fe7178c25c7a465d944f365dfe
geekfabio <geekfabio@gmail.com>
2022-02-14 15:41:28 +0100

    add dartz Either

--- lib/features/todo_pomodoro/domain/repositories/todo_repository.dart
+++ lib/features/todo_pomodoro/domain/repositories/todo_repository.dart
@@ -1,10 +1,12 @@
 import 'package:kt_dart/collection.dart';
+import 'package:todo_bloc/core/error/failure.dart';
 import 'package:todo_bloc/features/todo_pomodoro/domain/entities/todo_item.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class IRepositoryTodo {
-  Stream<KtList<TodoItem>> getAllTodos();
-  Stream<TodoItem> getTodoById(String id);
-  Stream<TodoItem> addTodo(TodoItem todo);
-  Stream<TodoItem> updateTodo(TodoItem todo);
-  Stream<TodoItem> deleteTodo(TodoItem todo);
+  Stream<Either<TodoFailure, KtList<TodoItem>>> getAllTodos();
+  Stream<Either<TodoFailure, TodoItem>> getTodoById(String id);
+  Stream<Either<TodoFailure, TodoItem>> addTodo(TodoItem todo);
+  Stream<Either<TodoFailure, TodoItem>> updateTodo(TodoItem todo);
+  Stream<Either<TodoFailure, TodoItem>> deleteTodo(TodoItem todo);
 }
