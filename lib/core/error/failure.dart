a6ac2bd9d3ff02a896f09fdc615448eaca19c5bb
geekfabio <geekfabio@gmail.com>
2022-02-14 15:41:36 +0100

    add TodoFailure

--- lib/core/error/failure.dart
+++ lib/core/error/failure.dart
@@ -10,3 +10,17 @@ class ServerFailure extends Failure {}
 
 ///Quando o erro é no Cache
 class CacheFailure extends Failure {}
+
+///Possiveis erros de requisiç]ao da Todo
+class TodoFailure extends Failure {
+  TodoFailure() : super();
+  factory TodoFailure.unexpected() {
+    return TodoFailure();
+  }
+  factory TodoFailure.insuficientPermission() {
+    return TodoFailure();
+  }
+  factory TodoFailure.errorDeleting() {
+    return TodoFailure();
+  }
+}
