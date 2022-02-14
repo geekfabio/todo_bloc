7a996ddc3d71c5dec25de6f8420469f2cdf0fbc7
geekfabio <geekfabio@gmail.com>
2022-02-14 15:22:55 +0100

    ♻ update UI

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:todo_bloc/todo_pomodoro/presentation/cubit/theme_cubit.dart';
-import 'package:todo_bloc/todo_pomodoro/presentation/screens/home/home_page.dart';
+import 'package:todo_bloc/features/todo_pomodoro/presentation/cubit/theme_cubit.dart';
+import 'package:todo_bloc/features/todo_pomodoro/presentation/screens/home/home_page.dart';
 
 import 'features/todo_pomodoro/shared/themes/theme.dart';
 
