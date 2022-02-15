1c26bee7049721c32fa17142a9794592db215258
geekfabio <geekfabio@gmail.com>
2022-02-15 23:50:24 +0100

    ♻TodoFailure to Failure

--- lib/features/todo_pomodoro/domain/repositories/todo_repository.dart
+++ lib/features/todo_pomodoro/domain/repositories/todo_repository.dart
@@ -3,10 +3,10 @@ import 'package:todo_bloc/core/error/failure.dart';
 import 'package:todo_bloc/features/todo_pomodoro/domain/entities/todo_item.dart';
 import 'package:dartz/dartz.dart';
 
-abstract class IRepositoryTodo {
-  Stream<Either<TodoFailure, KtList<TodoItem>>> getAllTodos();
-  Stream<Either<TodoFailure, TodoItem>> getTodoById(String id);
-  Stream<Either<TodoFailure, TodoItem>> addTodo(TodoItem todo);
-  Stream<Either<TodoFailure, TodoItem>> updateTodo(TodoItem todo);
-  Stream<Either<TodoFailure, TodoItem>> deleteTodo(TodoItem todo);
+abstract class ITodoRepository {
+  Future<Either<Failure, TodoItem>> getTodoById({required String id});
+  Future<Either<TodoFailure, KtList<TodoItem>>> getAllTodos();
+  Future<Either<TodoFailure, TodoItem>> addTodo(TodoItem todo);
+  Future<Either<TodoFailure, TodoItem>> updateTodo(TodoItem todo);
+  Future<Either<TodoFailure, TodoItem>> deleteTodo(TodoItem todo);
 }
