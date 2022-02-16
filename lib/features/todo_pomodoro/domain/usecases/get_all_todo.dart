51a378695fdd1e8cab3703f437568191a5c54cbd
geekfabio <geekfabio@gmail.com>
2022-02-16 23:11:53 +0100

    add GetAllTodo with NoParams

--- lib/features/todo_pomodoro/domain/usecases/get_all_todo.dart
+++ lib/features/todo_pomodoro/domain/usecases/get_all_todo.dart
@@ -1,13 +1,22 @@
+import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
+import 'package:kt_dart/collection.dart';
+import 'package:todo_bloc/core/error/failure.dart';
+import 'package:todo_bloc/core/usecases/usecase.dart';
+import 'package:todo_bloc/features/todo_pomodoro/domain/entities/todo_item.dart';
 import 'package:todo_bloc/features/todo_pomodoro/domain/repositories/todo_repository.dart';
 
-class GetAllTodo extends Equatable {
+class GetAllTodo extends UseCase<KtList<TodoItem>, NoParams> {
   final ITodoRepository repository;
   GetAllTodo({required this.repository});
 
-  //Call(ITodoRepository repository) => repository.getAllTodo()),
-
   @override
-  // TODO: implement props
-  List<Object?> get props => throw UnimplementedError();
+  Future<Either<Failure, KtList<TodoItem>>> call(NoParams params) {
+    return repository.getAllTodos();
+  }
+}
+
+class NoParams extends Equatable {
+  @override
+  List<Object?> get props => [];
 }
