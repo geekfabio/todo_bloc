0691077b5ba80ea206e5e49b8ef321288f37d85f
geekfabio <geekfabio@gmail.com>
2022-02-16 23:11:11 +0100

    modify TodoFailure to TodoItem

--- lib/features/todo_pomodoro/domain/repositories/todo_repository.dart
+++ lib/features/todo_pomodoro/domain/repositories/todo_repository.dart
@@ -1,12 +1,12 @@
+import 'package:dartz/dartz.dart';
 import 'package:kt_dart/collection.dart';
 import 'package:todo_bloc/core/error/failure.dart';
 import 'package:todo_bloc/features/todo_pomodoro/domain/entities/todo_item.dart';
-import 'package:dartz/dartz.dart';
 
 abstract class ITodoRepository {
   Future<Either<Failure, TodoItem>> getTodoById({required String id});
-  Future<Either<TodoFailure, KtList<TodoItem>>> getAllTodos();
-  Future<Either<TodoFailure, TodoItem>> addTodo(TodoItem todo);
-  Future<Either<TodoFailure, TodoItem>> updateTodo(TodoItem todo);
-  Future<Either<TodoFailure, TodoItem>> deleteTodo(TodoItem todo);
+  Future<Either<Failure, KtList<TodoItem>>> getAllTodos();
+  Future<Either<Failure, TodoItem>> addTodo(TodoItem todo);
+  Future<Either<Failure, TodoItem>> updateTodo(TodoItem todo);
+  Future<Either<Failure, TodoItem>> deleteTodo(TodoItem todo);
 }
