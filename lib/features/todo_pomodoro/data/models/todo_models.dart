22eb57a4014232ee758f4687231f18d947ea54a7
geekfabio <geekfabio@gmail.com>
2022-02-18 19:42:04 +0100

    add TodoModel on data

--- lib/features/todo_pomodoro/data/models/todo_models.dart
+++ lib/features/todo_pomodoro/data/models/todo_models.dart
@@ -1,11 +1,25 @@
-import '../../domain/entities/todo_item.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:todo_bloc/features/todo_pomodoro/domain/entities/todo_item.dart';
 
 class TodoModel extends TodoItem {
-  const TodoModel()
-      : super(
-          id: '1',
-          title: 'title',
-          dateCreated: 'description',
-          isDone: false,
+  const TodoModel({
+    required String id,
+    required String title,
+    required String dateCreated,
+    required bool isDone,
+    String dateFinish = "",
+    String dateToStart = "",
+    String project = "Task",
+    Color color = Colors.blue,
+  }) : super(
+          id: id,
+          title: title,
+          dateCreated: dateCreated,
+          isDone: isDone,
+          dateFinish: dateFinish,
+          dateToStart: dateToStart,
+          project: project,
+          color: color,
         );
 }
