8b103d93fce6db11a91582b16d494417851d7aec
geekfabio <geekfabio@gmail.com>
2022-02-24 22:59:44 +0100

    ♻

--- lib/features/todo_pomodoro/domain/repositories/todo_repository.dart
+++ lib/features/todo_pomodoro/domain/repositories/todo_repository.dart
@@ -1,13 +1,12 @@
 import 'package:dartz/dartz.dart';
 import 'package:kt_dart/collection.dart';
-
-import '../../../../core/error/failure.dart';
-import '../entities/todo_item.dart';
+import 'package:todo_bloc/core/error/failure.dart';
+import 'package:todo_bloc/features/todo_pomodoro/domain/entities/todo_entity.dart';
 
 abstract class TodoRepository {
-  Future<Either<Failure, TodoItem>> getTodoById({required String id});
-  Future<Either<Failure, KtList<TodoItem>>> getAllTodos();
-  Future<Either<Failure, bool>> addTodo(TodoItem todo);
-  Future<Either<Failure, TodoItem>> updateTodo(TodoItem todo);
-  Future<Either<Failure, TodoItem>> deleteTodo(TodoItem todo);
+  Future<Either<Failure, TodoEntity>> getTodoById({required String id});
+  Future<Either<Failure, KtList<TodoEntity>>> getAllTodos();
+  Future<Either<Failure, bool>> addTodo(TodoEntity todo);
+  Future<Either<Failure, TodoEntity>> updateTodo(TodoEntity todo);
+  Future<Either<Failure, TodoEntity>> deleteTodo(TodoEntity todo);
 }
