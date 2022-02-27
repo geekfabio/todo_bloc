e90c3c34c42c75965a958882389baee33f820b38
geekfabio <geekfabio@gmail.com>
2022-02-27 13:44:33 +0100

    added package internet_connection_checker

--- test/core/services/network_info_test.dart
+++ test/core/services/network_info_test.dart
@@ -1,9 +1,10 @@
-import 'package:data_connection_checker/data_connection_checker.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:todo_bloc/core/services/network_info.dart';
 
-class MockDataConnectionChecker extends Mock implements DataConnectionChecker {}
+class MockDataConnectionChecker extends Mock
+    implements InternetConnectionChecker {}
 
 void main() {
   late MockDataConnectionChecker mockDataConnectionChecker;
@@ -17,9 +18,8 @@ void main() {
   test("Test if device have a connection with internet", () async* {
     when(() => mockDataConnectionChecker.hasConnection)
         .thenAnswer((_) async => true);
-
     //act
-    final result = networkInfoImp.isConnected;
+    final result = await networkInfoImp.isConnected;
     expect(result, true);
     verifyNoMoreInteractions(mockDataConnectionChecker);
   });
