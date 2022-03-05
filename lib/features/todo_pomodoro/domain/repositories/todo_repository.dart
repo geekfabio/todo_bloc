5de5dfbd80b0e7c73330b814de88a095bd67fd79
geekfabio <geekfabio@gmail.com>
2022-03-05 23:05:52 +0100

    added method getTodoByProject

--- lib/features/todo_pomodoro/domain/repositories/todo_repository.dart
+++ lib/features/todo_pomodoro/domain/repositories/todo_repository.dart
@@ -5,6 +5,7 @@ import 'package:todo_bloc/features/todo_pomodoro/domain/entities/todo_entity.dar
 abstract class TodoRepository {
   Future<Either<Failure, TodoEntity>> getTodoById({required String id});
   Future<Either<Failure, List<TodoEntity>>> getAllTodos();
+  Future<Either<Failure, List<TodoEntity>>> getTodoByProject(String project);
   Future<Either<Failure, bool>> addTodo(TodoEntity todo);
   Future<Either<Failure, TodoEntity>> updateTodo(TodoEntity todo);
   Future<Either<Failure, TodoEntity>> deleteTodo(TodoEntity todo);
