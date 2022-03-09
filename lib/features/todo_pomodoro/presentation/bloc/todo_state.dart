f526701c8ecc4fff1d396a3afae40c71413c405a
geekfabio <geekfabio@gmail.com>
2022-03-09 23:48:24 +0100

    class state done

--- lib/features/todo_pomodoro/presentation/bloc/todo_state.dart
+++ lib/features/todo_pomodoro/presentation/bloc/todo_state.dart
@@ -2,9 +2,25 @@ part of 'todo_bloc.dart';
 
 abstract class TodoState extends Equatable {
   const TodoState();
-  
+
   @override
   List<Object> get props => [];
 }
 
 class TodoInitial extends TodoState {}
+
+class TodoLoading extends TodoState {}
+
+class TodoLoadedByProject extends TodoState {
+  final List<TodoModel> todos;
+  const TodoLoadedByProject({required this.todos});
+  @override
+  List<Object> get props => [todos];
+}
+
+class TodoLoadedAll extends TodoState {
+  final List<TodoModel> todos;
+  const TodoLoadedAll({required this.todos});
+  @override
+  List<Object> get props => [todos];
+}
