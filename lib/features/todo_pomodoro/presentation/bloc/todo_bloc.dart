9d26c16c3725e5545a01951dbb6ff68760d6c232
geekfabio <geekfabio@gmail.com>
2022-03-09 23:48:05 +0100

    emit events done

--- lib/features/todo_pomodoro/presentation/bloc/todo_bloc.dart
+++ lib/features/todo_pomodoro/presentation/bloc/todo_bloc.dart
@@ -1,14 +1,28 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:todo_bloc/features/todo_pomodoro/presentation/screens/models/todox_model.dart';
+import 'package:todo_bloc/features/todo_pomodoro/data/models/todo_model.dart';
 
 part 'todo_event.dart';
 part 'todo_state.dart';
 
 class TodoBloc extends Bloc<TodoEvent, TodoState> {
+  List<TodoModel> list = List.empty(growable: true);
+
   TodoBloc() : super(TodoInitial()) {
     on<TodoEvent>((event, emit) {
-      // TODO: implement event handler
+      if (event is TodoAdded) {
+        list.add(event.todo);
+        emit(TodoLoadedByProject(todos: list));
+      } else if (event is TodoDeleted) {
+        list.remove(event.todo);
+        emit(TodoLoadedByProject(todos: list));
+      } else if (event is TodoUpdated) {
+        list.remove(event.todo);
+        list.add(event.todo);
+        emit(TodoLoadedByProject(todos: list));
+      } else if (event is TodoLoadedByProject) {
+        emit(TodoLoadedByProject(todos: list));
+      }
     });
   }
 }
