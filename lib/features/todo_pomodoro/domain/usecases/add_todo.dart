b4629c5f2e9da2f743e34412a24408fa2a3987d0
geekfabio <geekfabio@gmail.com>
2022-03-12 21:44:36 +0100

    rename to AddTodoUseCase

--- lib/features/todo_pomodoro/domain/usecases/add_todo.dart
+++ lib/features/todo_pomodoro/domain/usecases/add_todo.dart
@@ -4,9 +4,9 @@ import 'package:todo_bloc/core/usecases/usecase.dart';
 import 'package:todo_bloc/features/todo_pomodoro/domain/entities/todo_entity.dart';
 import 'package:todo_bloc/features/todo_pomodoro/domain/repositories/todo_repository.dart';
 
-class AddTodo implements UseCase<bool, TodoEntity> {
+class AddTodoUseCase implements UseCase<bool, TodoEntity> {
   final TodoRepository repository;
-  AddTodo({required this.repository});
+  AddTodoUseCase({required this.repository});
 
   @override
   Future<Either<Failure, bool>> call(TodoEntity params) async =>
