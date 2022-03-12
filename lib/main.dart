bffb090c54378a15b64e34c8a2e6ea4b8d9b0571
geekfabio <geekfabio@gmail.com>
2022-03-12 21:45:14 +0100

    await depency injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_bloc/injection_container.dart' as di;
 import 'package:todo_bloc/my_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
   BlocOverrides.runZoned(
     () => runApp(const MyApp()),
     blocObserver: AppBlocObserver(),
