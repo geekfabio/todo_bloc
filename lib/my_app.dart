4a39732fe3a64e88a7756b66dc78c48dd69f758c
geekfabio <geekfabio@gmail.com>
2022-03-19 02:06:47 +0100

    current themeMode provid by depency injection

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -4,19 +4,20 @@ import 'package:todo_bloc/features/todo_pomodoro/presentation/cubit/theme_cubit.
 import 'package:todo_bloc/features/todo_pomodoro/presentation/screens/home/home_page.dart';
 
 import 'features/todo_pomodoro/shared/themes/theme.dart';
+import 'injection_container.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (_) => ThemeCubit(),
+      create: (_) => sl<ThemeCubit>(),
       child: BlocBuilder<ThemeCubit, ThemeState>(
         builder: (context, state) {
           return MaterialApp(
             theme: AppTheme.lightTheme,
             //    themeMode: context.select((ThemeCubit cubit) => cubit.state.mode),
-            themeMode: ThemeMode.dark,
+            themeMode: sl<ThemeCubit>().state.mode,
             darkTheme: AppTheme.lightTheme,
             title: 'Todo Bloc',
             home: const HomePage(),
