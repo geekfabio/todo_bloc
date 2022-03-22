f3d78c1a95e062fa3cff5e36f4ddd9cac3f3fa8b
geekfabio <geekfabio@gmail.com>
2022-03-22 23:56:55 +0100

    ♻ added GetAllTodo and fix bugs

--- lib/features/todo_pomodoro/presentation/bloc/todo_bloc.dart
+++ lib/features/todo_pomodoro/presentation/bloc/todo_bloc.dart
@@ -3,7 +3,9 @@ import 'package:equatable/equatable.dart';
 import 'package:todo_bloc/core/error/failure.dart';
 import 'package:todo_bloc/core/usecases/usecase.dart';
 import 'package:todo_bloc/features/todo_pomodoro/data/models/todo_model.dart';
+import 'package:todo_bloc/features/todo_pomodoro/domain/entities/todo_entity.dart';
 import 'package:todo_bloc/features/todo_pomodoro/domain/usecases/add_todo.dart';
+import 'package:todo_bloc/features/todo_pomodoro/domain/usecases/get_all_todo.dart';
 import 'package:todo_bloc/features/todo_pomodoro/domain/usecases/update_todo.dart';
 
 part 'todo_event.dart';
@@ -12,41 +14,60 @@ part 'todo_state.dart';
 class TodoBloc extends Bloc<TodoEvent, TodoState> {
   final AddTodoUseCase addTodoUseCase;
   final UpdateTodo updateTodoUseCase;
+  final GetAllTodo getAllTodo;
 
-//  List<TodoModel> listTodoModel = [];
   final listTodo = <TodoModel>[];
   int get listCount => listTodo.length;
-  TodoBloc({required this.updateTodoUseCase, required this.addTodoUseCase})
-      : super(TodoInitial()) {
+  TodoBloc(
+      {required this.getAllTodo,
+      required this.updateTodoUseCase,
+      required this.addTodoUseCase})
+      : super(TodoLoadingState()) {
     // ignore: void_checks
+
+    on<TodoFetchList>((event, emit) async {
+      try {
+        final result = await getAllTodo(NoParams());
+        if (result.isRight()) {
+          List<TodoEntity> listEntity = result.getOrElse(() => []);
+          listTodo.addAll(listEntity.map((e) => TodoModel.fromTodoEntity(e)));
+          emit(TodoLoadedState(listTodo: listTodo));
+        } else {
+          emit(const TodoErrorState(errorMessage: "Error adding todo"));
+        }
+      } on CacheFailure {
+        emit(const TodoErrorState(errorMessage: "Error adding todo"));
+      }
+    });
     on<TodoAdded>((event, emit) async {
-      emit(TodoLoading());
       try {
         final result = await addTodoUseCase(event.todo);
         if (result.isRight()) {
           listTodo.add(TodoModel.fromTodoEntity(event.todo));
-          emit(TodoLoaded(todos: listTodo));
+          emit(TodoLoadedState(listTodo: listTodo));
         } else {
-          emit(TodoInitial());
+          emit(const TodoErrorState(errorMessage: "Error adding todo"));
         }
       } on CacheFailure {
-        emit(TodoInitial());
+        emit(const TodoErrorState(errorMessage: "Error adding todo"));
       }
     });
     on<TodoUpdated>((event, emit) async {
-      emit(TodoLoading());
-      Future.delayed(const Duration(seconds: 4));
       try {
         final result =
             await updateTodoUseCase(ParamsTodoItem(todo: event.todo));
         if (result.isRight()) {
-          listTodo.add(TodoModel.fromTodoEntity(event.todo));
-          emit(TodoLoaded(todos: listTodo));
+          //remove da lista
+          listTodo.removeWhere((todo) => todo.id == event.todo.id);
+          //add na lista o item atualizado
+          listTodo.add(event.todo);
+          emit(TodoLoadedState(listTodo: listTodo));
         } else {
-          emit(TodoInitial());
+          emit(const TodoErrorState(errorMessage: "Update todo failed"));
         }
       } on CacheFailure {
-        emit(TodoInitial());
+        emit(const TodoErrorState(
+            errorMessage: "updateTodoUseCase todo failed"));
       }
     });
   }
